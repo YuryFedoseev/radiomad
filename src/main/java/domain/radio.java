971baaf8398dd37fcae8f7fd21707e6859ae95a1
Yury.Fedoseev <Yri95@inbox.ru>
2021-11-18 23:08:52 +0300

    Переделка радиолюбителя

---
+++ src/main/java/domain/radio.java
@@ -0,0 +1,64 @@
+package domain;
+
+public class Radio {
+    private int maxChanel;
+    private int minChanel;
+    private int maxVolume;
+    private int minVolume;
+    private int currentVolume;
+    private int currentChanel;
+
+    public void setMaxChanel() {
+        maxChanel = 9;
+    }
+
+    public void setMinChanel() {
+        minChanel = 0;
+    }
+
+    public void setMaxVolume() {
+        maxVolume = 10;
+    }
+
+    public void setMinVolume() {
+        minVolume = 0;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume >= maxVolume) {
+            return;
+        else if(currentVolume < maxVolume) {
+                currentVolume = currentVolume +1;
+            }
+        }
+    }
+    public void loweringVolume(){
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        else if(currentVolume <= minVolume) {
+                return;
+            }
+        }
+    }
+    public void increaseChanel(){
+        if (currentChanel >= 9) {
+            currentChanel = minChanel;
+        else if (currentChanel < 9) {
+                currentChanel = currentChanel +1;
+
+            }
+        }
+    }
+    public void loweringChanel(){
+        if (currentChanel > 0 ) {
+            currentChanel = minChanel;
+        else if (currentChanel < 10) {
+                currentChanel = currentChanel +1;
+
+            }
+        }
+
+    }
+}
+
+
